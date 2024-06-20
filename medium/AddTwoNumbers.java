e6018e76f254a630a18d79257fb449f5ea09c211
krishnarajkumar0610 <krishnaraj0610@gmail.com>
2024-06-20 19:43:24 +0530

    Problem 2

---
+++ medium/AddTwoNumbers.java
@@ -0,0 +1,56 @@
+package medium;
+
+public class AddTwoNumbers {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(); // Dummy node to simplify result list handling
+        ListNode curr = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null) {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+            int sum = x + y + carry;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (l1 != null)
+                l1 = l1.next;
+            if (l2 != null)
+                l2 = l2.next;
+        }
+
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        AddTwoNumbers obj = new AddTwoNumbers();
+        ListNode result = obj.addTwoNumbers(l1, l2);
+        while (result != null) {
+            System.out.print(result.val + "->");
+            result = result.next;
+        }
+        System.out.println("End");
+    }
+}
