5366263dc85eb14ea1c7cdd1f5836fa6481512fb
krishnarajkumar0610 <krishnaraj0610@gmail.com>
2024-06-21 17:43:46 +0530

    Problem 58

--- easy/LengthOfLastWord.java
+++ easy/LengthOfLastWord.java
@@ -3,10 +3,11 @@ package easy;
 public class LengthOfLastWord {
     public static void main(String[] args) {
         String s = "Hello world";
+        lengthOfLastWord(s);
     }
 
     public static int lengthOfLastWord(String s) {
         String[] words = s.trim().split(" ");
-        return  words[words.length - 1].length();
+        return words[words.length - 1].length();
     }
 }
