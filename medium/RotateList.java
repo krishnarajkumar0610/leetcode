357a8e744fad2a671cd192ea700bf542bb969621
krishnarajkumar0610 <krishnaraj0610@gmail.com>
2024-06-22 13:16:12 +0530

    Problem 61

---
+++ medium/RotateList.java
@@ -0,0 +1,57 @@
+package medium;
+
+import java.util.List;
+
+import org.w3c.dom.NodeList;
+
+public class RotateList {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode rotateList(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0)
+            return head;
+        int length = 1;
+        ListNode tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+            length++;
+        }
+        length++;
+        if (k % length == 0)
+            return head;
+        ListNode newTail = head;
+        for (int i = 1; i < length - k; i++) {
+            newTail = newTail.next;
+        }
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+        tail.next = head;        
+        return newHead;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(5, new ListNode(4, new ListNode(3, new ListNode(2, new ListNode(1)))));
+        RotateList list = new RotateList();
+        head = list.rotateList(head, 3);
+        while (head != null) {
+            System.out.print(head.val + "->");
+            head = head.next;
+        }
+        System.out.print("End");
+    }
+}
