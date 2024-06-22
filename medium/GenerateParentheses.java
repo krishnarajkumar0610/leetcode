4a0c4a10c737fec030550ebeec1437e9c3d7cd8e
krishnarajkumar0610 <krishnaraj0610@gmail.com>
2024-06-22 11:22:39 +0530

    Problem 22

---
+++ medium/GenerateParentheses.java
@@ -0,0 +1,32 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParentheses {
+    public static void main(String[] args) {
+        int n = 3;
+        List<String> result = generateParenthesis(n);
+        System.out.println(result);
+    }
+
+    public static List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        generateParenthesisHelper(result, "", 0, 0, n);
+        return result;
+    }
+
+    private static void generateParenthesisHelper(List<String> result, String current, int open, int close, int max) {
+        if (current.length() == max * 2) {
+            result.add(current);
+            return;
+        }
+
+        if (open < max) {
+            generateParenthesisHelper(result, current + "(", open + 1, close, max);
+        }
+        if (close < open) {
+            generateParenthesisHelper(result, current + ")", open, close + 1, max);
+        }
+    }
+}
