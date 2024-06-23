957d164b07ff3f11deedb0f50d869f6b74725599
krishnarajkumar0610 <krishnaraj0610@gmail.com>
2024-06-23 19:07:22 +0530

    Problem 70

--- easy/ClimbingStairs.java
+++ easy/ClimbingStairs.java
@@ -2,22 +2,17 @@ package easy;
 
 class ClimbingStairs {
     public static void main(String[] args) {
-        int n = 3; // You can change this to test other values of n
+        int n = 3;
         System.out.println(climbStairs(n));
     }
 
     public static int climbStairs(int n) {
-        // Base cases
         if (n < 0) {
             return 0;
         }
-        if (n == 0) {
+        if (n == 0 || n == 1) {
             return 1;
         }
-        if (n == 1) {
-            return 1;
-        }
-        // Recursive case
         return climbStairs(n - 1) + climbStairs(n - 2);
     }
 }
