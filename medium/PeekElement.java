c8c5fa82302955a5813bef0d130d50222f4cf677
krishnarajkumar0610 <krishnaraj0610@gmail.com>
2024-06-24 18:33:05 +0530

    Problem 162

---
+++ medium/PeekElement.java
@@ -0,0 +1,24 @@
+package medium;
+
+class PeekElement {
+    public static int findPeakElement(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = { 1, 2, 3, 1 };
+
+        System.out.println(findPeakElement(nums1)); // Expected Output: 1
+    }
+}
