59fc5953494483495a1ab4a0f67e04b77e1a5212
krishnarajkumar0610 <krishnaraj0610@gmail.com>
2024-06-26 17:48:29 +0530

    Problem 28

---
+++ easy/FirstOccurence.java
@@ -0,0 +1,14 @@
+package easy;
+
+public class FirstOccurence {
+    public static void main(String[] args) {
+        // System.out.println(strStr("leetcode", "leeto"));
+        String a = "krishna";
+        String b = "ra";
+        int index = a.indexOf(b);
+        System.out.println(index);
+    }
+
+    public static void strStr(String haystack, String needle) {
+    }
+}
