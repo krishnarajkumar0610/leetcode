b2d760d20a8d168fdc6a2d314f985b7d0cb2788f
krishnarajkumar0610 <krishnaraj0610@gmail.com>
2024-06-28 19:34:58 +0530

    Problem 2540

---
+++ easy/MinimumComValue.java
@@ -0,0 +1,27 @@
+package easy;
+
+public class MinimumComValue {
+    public static void main(String[] args) {
+        int[] nums1 = {};
+        int[] numw2 = {};
+        System.out.println(findMinValue(nums1,numw2));
+    }
+    public static int findMinValue(int[] nums1,int[] nums2){
+            int p1 = 0, p2 = 0;
+            int minCommon = Integer.MAX_VALUE;
+            
+            while (p1 < nums1.length && p2 < nums2.length) {
+                if (nums1[p1] == nums2[p2]) {
+                    minCommon = Math.min(minCommon, nums1[p1]);
+                    p1++;
+                    p2++;
+                } else if (nums1[p1] < nums2[p2]) {
+                    p1++;
+                } else {
+                    p2++;
+                }
+            }
+            
+            return minCommon == Integer.MAX_VALUE ? -1 : minCommon;     
+    }
+}
