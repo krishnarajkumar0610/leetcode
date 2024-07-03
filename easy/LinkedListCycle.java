bd2b0029e95d1fef7c83ceecc441b109ecaadcec
krishnarajkumar0610 <krishnaraj0610@gmail.com>
2024-07-03 23:19:13 +0530

    Problem 141

---
+++ easy/LinkedListCycle.java
@@ -0,0 +1,24 @@
+public class LinkedListCycle {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        ListNode tortoise = head;
+        ListNode rabbit = head;
+        while (rabbit != null && rabbit.next != null) {
+            tortoise = tortoise.next;
+            rabbit = rabbit.next.next;
+            if (tortoise == rabbit)
+                return true;
+        }
+        return false;
+    }
+}
