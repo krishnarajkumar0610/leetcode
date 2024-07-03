e306c9bcdba705e250d10b4538bec94e0e1df911
krishnarajkumar0610 <krishnaraj0610@gmail.com>
2024-07-03 20:51:02 +0530

    Problem 54

--- medium/Spiralarr.java
+++ medium/Spiralarr.java
@@ -18,31 +18,31 @@ public class Spiralarr {
         if (matrix == null || matrix.length == 0) {
             return result;
         }
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        int rowStart = 0, rowEnd = rows - 1;
-        int colStart = 0, colEnd = cols - 1;
-        while (rowStart <= rowEnd && colStart <= colEnd) {
-            for (int i = colStart; i <= colEnd; i++) {
+
+        int length = matrix.length;
+
+        for (int[] m : matrix) {
+            if (m.length != length) {
+                return new ArrayList<>();
+            }
+        }
+
+        int rowStart = 0, rowEnd = matrix.length;
+        int colStart = 0, colEnd = matrix[0].length;
+
+        while (rowStart < rowEnd && colStart < colEnd) {
+            for (int i = colStart; i < colEnd; i++)
                 result.add(matrix[rowStart][i]);
-            }
             rowStart++;
-            for (int i = rowStart; i <= rowEnd; i++) {
-                result.add(matrix[i][colEnd]);
-            }
+            for (int i = rowStart; i < rowEnd; i++)
+                result.add(matrix[i][colEnd - 1]);
             colEnd--;
-            if (rowStart <= rowEnd) {
-                for (int i = colEnd; i >= colStart; i--) {
-                    result.add(matrix[rowEnd][i]);
-                }
-                rowEnd--;
-            }
-            if (colStart <= colEnd) {
-                for (int i = rowEnd; i >= rowStart; i--) {
-                    result.add(matrix[i][colStart]);
-                }
-                colStart++;
-            }
+            for (int i = colEnd - 1; i >= colStart; i--)
+                result.add(matrix[rowEnd - 1][i]);
+            rowEnd--;
+            for (int i = rowEnd - 1; i >= rowStart; i--)
+                result.add(matrix[i][colStart]);
+            colStart++;
         }
         return result;
     }
