8f7bbc5dbb922f7c735fbbc9d27e0650130466c4
krishnarajkumar0610 <krishnaraj0610@gmail.com>
2024-07-05 11:01:10 +0530

    Problem 3

--- medium/LongestSubStringNoDup.java
+++ medium/LongestSubStringNoDup.java
@@ -9,20 +9,18 @@ public class LongestSubStringNoDup {
     }
 
     public static int findSubString(String s) {
-        int n = s.length();
+        HashSet<Character> set = new HashSet<>();
         int maxLength = 0;
-        int delete = 0;
-        Set<Character> set = new HashSet<>();
-        for (int i = 0; i < n; i++) {
-            while (set.contains(s.charAt(i))) {
-                set.remove(s.charAt(delete));
-                delete++;
+        int left = 0;
+        for (int right = 0; right < s.length(); right++) {
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
             }
-            set.add(s.charAt(i));
-            maxLength = Math.max(maxLength, i - delete + 1);
+            set.add(s.charAt(right));
+            maxLength = Math.max(maxLength, right - left + 1);
+            System.out.println("Current set:" + set);
         }
-        System.out.println(set);
         return maxLength;
     }
-
 }
