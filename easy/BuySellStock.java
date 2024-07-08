957be0924be4ab18c780c44873c117da66255fe5
krishnarajkumar0610 <krishnaraj0610@gmail.com>
2024-07-08 20:25:12 +0530

    Problem 121

---
+++ easy/BuySellStock.java
@@ -0,0 +1,27 @@
+package easy;
+
+import java.util.Arrays;
+
+public class BuySellStock {
+    public static void main(String[] args) {
+        int[] stock = { 2, 4, 1 };
+        System.out.println(findMaxProfit(stock));
+    }
+
+    public static int findMaxProfit(int[] prices) {
+        if (prices.length == 0 || prices == null)
+            return 0;
+        int minPrice = Integer.MAX_VALUE;
+        int maxPrice = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice)
+                minPrice = prices[i];
+            else {
+                int profit = prices[i] - minPrice;
+                if (profit > maxPrice)
+                    maxPrice = profit;
+            }
+        }
+        return maxPrice;
+    }
+}
