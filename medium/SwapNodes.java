c5cfe52f719c5b1a63fe4c32e5baaeaca75000d3
krishnarajkumar0610 <krishnaraj0610@gmail.com>
2024-07-09 19:22:59 +0530

    Problem 24

---
+++ medium/SwapNodes.java
@@ -0,0 +1,36 @@
+package medium;
+
+public class SwapNodes {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        while (prev.next != null && prev.next.next != null) {
+            ListNode first = prev.next;
+            ListNode second = first.next;
+            first.next = second.next;
+            second.next = first;
+            prev.next = second;
+            prev = first;
+        }
+        return dummy.next;
+    }
+}
