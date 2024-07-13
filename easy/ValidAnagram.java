d512f2fb19eefa8543cf5de4b4691dc2f3fd8c25
krishnarajkumar0610 <krishnaraj0610@gmail.com>
2024-07-14 00:23:51 +0530

    Problem 242

---
+++ easy/ValidAnagram.java
@@ -0,0 +1,25 @@
+package easy;
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+    public static void main(String[] args) {
+        ValidAnagram v = new ValidAnagram();
+        System.out.println(v.isAnagram("anagram", "nagaram"));
+    }
+
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] c1 = s.toCharArray();
+        char[] c2 = t.toCharArray();
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+        for (int i = 0; i < c1.length; i++) {
+            if (c1[i] != c2[i])
+                return false;
+        }
+        return true;
+    }
+}
